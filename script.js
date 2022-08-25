a515503c66f6ceab83f618d853ba9ba91ec8a69d
saurabhced786 <saurabhgupta@cedcommerce.com>
2022-08-25 13:28:11 +0530

    Git Profile Update Version 0.0.1

---
+++ script.js
@@ -0,0 +1,35 @@
+     
+
+function filterData(){
+   const searchInp = document.getElementById('searchUser').value;
+   gitApiData();   
+   async function gitApiData(){
+    const gitUser = await fetch('https://api.github.com/users');
+       allUser =  await gitUser.json();
+        allUser.forEach(userData => {
+           //console.log(userData.login);
+           if(userData.login == searchInp){
+                //Show Profile popup
+                document.getElementsByClassName('profile-card').style.dispay="block";
+                singleGitUsers();
+                async function singleGitUsers(){
+                    const mainUser = await fetch(`https://api.github.com/users/${userData.login}`);
+                    singleUser =  await mainUser.json();
+                    // Data Fromatting
+                    document.getElementById('proPic').src = singleUser.avatar_url;
+                    document.getElementById('userName').innerHTML = singleUser.login;
+                    document.getElementById('gitFollowers').innerHTML =singleUser.followers+' Followers';
+                    document.getElementById('gitFollowing').innerHTML =singleUser.following+' Following'; 
+                    document.getElementById('gitRepos').innerHTML =singleUser.public_repos+' Repos';
+                }
+
+           }
+        });     
+   }
+}
+
+function showProfile(data){ 
+    console.log(data,"mydata");   
+//enter name data url in here
+document.getElementById('profile').innerHTML =data;
+};
