296954c87848d2ea9a6dbfd08a7622d00b853e2d
saurabhced786 <saurabhgupta@cedcommerce.com>
2022-08-26 16:59:23 +0530

    Update git Profile 0.1.1

--- script.js
+++ script.js
@@ -1,39 +1,47 @@
-     
-var proPic;
-var userName;
-var gitFollowers;
-var gitFollowing;
-var gitRepos;
-function filterData(){
+function filterData() {
    const searchInp = document.getElementById('searchUser').value;
-   gitApiData();   
-   async function gitApiData(){
-    const gitUser = await fetch('https://api.github.com/users');
-       allUser =  await gitUser.json();
-        allUser.forEach(userData => {
-           //console.log(userData.login);
-           if(userData.login == searchInp){
-                //Show Profile popup
-                document.getElementById('profile-card').style.display='block';
-                singleGitUsers();
-                async function singleGitUsers(){
-                    const mainUser = await fetch(`https://api.github.com/users/${userData.login}`);
-                    singleUser =  await mainUser.json();
-                    // Data Fromatting
-                   proPic = document.getElementById('proPic').src = singleUser.avatar_url;
-                   userName = document.getElementById('userName').innerHTML = singleUser.login;
-                   gitFollowers = document.getElementById('gitFollowers').innerHTML =singleUser.followers+' Followers';
-                   gitFollowing = document.getElementById('gitFollowing').innerHTML =singleUser.following+' Following'; 
-                   gitRepos = document.getElementById('gitRepos').innerHTML =singleUser.public_repos+' Repos';
-                   showProfile(proPic,userName,gitFollowers,gitFollowing,gitRepos);
-                }
-
-           }
-        });     
+   gitApiData();
+   async function gitApiData() {
+      const gitUser = await fetch('https://api.github.com/users');
+      allUser = await gitUser.json();
+      allUser.forEach(userData => {
+         //console.log(userData.login);
+         if (userData.login == searchInp || searchInp.includes(userData.login)) {
+            //Show Profile popup
+            document.getElementById('profile-card').style.display = 'block';
+            singleGitUsers();
+            async function singleGitUsers() {
+               const mainUser = await fetch(`https://api.github.com/users/${userData.login}`);
+               singleUser = await mainUser.json();
+               // Data Fromatting
+               proPic = document.getElementById('proPic').src = singleUser.avatar_url;
+               userName = document.getElementById('userName').innerHTML = singleUser.login;
+               gitFollowers = document.getElementById('gitFollowers').innerHTML = singleUser.followers + ' Followers';
+               gitFollowing = document.getElementById('gitFollowing').innerHTML = singleUser.following + ' Following';
+               gitRepos = document.getElementById('gitRepos').innerHTML = singleUser.public_repos;
+               gitName = singleUser.name;
+               gitCompany = singleUser.company;
+               gitLoacation = singleUser.location;
+               gitMail = singleUser.email;
+               gitBlog = singleUser.blog;
+               gitTwitter = singleUser.twitter_username;
+               showProfile(proPic, gitName, userName, gitFollowers, gitFollowing, gitRepos, gitCompany, gitLoacation, gitMail, gitBlog, gitTwitter);
+            }
+         }
+      });
    }
 }
 
-function showProfile(proPic,userName,gitFollowers,gitFollowing,gitRepos){ 
-   console.log(proPic,"mydata");
-   document.getElementById('gitproPic').src = proPic; 
-   };
+function showProfile(proPic, gitName, userName, gitFollowers, gitFollowing, gitRepos, gitCompany, gitLoacation, gitMail, gitBlog, gitTwitter) {
+   window.localStorage.setItem("profilePic", proPic);
+   window.localStorage.setItem("gitName", gitName);
+   window.localStorage.setItem("userName", userName);
+   window.localStorage.setItem("gitfollowers", gitFollowers);
+   window.localStorage.setItem("gitfollowing", gitFollowing);
+   window.localStorage.setItem("gitRepos", gitRepos);
+   window.localStorage.setItem("gitCompany", gitCompany);
+   window.localStorage.setItem("gitLocation", gitLoacation);
+   window.localStorage.setItem("gitMail", gitMail);
+   window.localStorage.setItem("gitBlog", gitBlog);
+   window.localStorage.setItem("gitTwitter",gitTwitter);
+};
